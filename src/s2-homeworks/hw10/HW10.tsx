9b0b08e7bfda1d0336a497ba8efb5ce5f2f21362
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-08-26 00:36:38 +0300

    add hw2-10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import {loadingAC, loadingReducer} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
+import {CombinedState} from "redux";
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -15,11 +16,15 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch()
 
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+console.log(isLoading)
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
+        dispatch(loadingAC(true))
 
+        setTimeout(function() {(dispatch(loadingAC(false)))}, 1500)
         // setTimeout
     }
 
